6835847ee621b895a337cdde513e0b3b85640755
Brandon G. Neri <arcaniteamp@gmail.com>
2019-03-11 16:52:07 -0600

    EpsiDev Galaxy Automatic Deploy

--- src/app/shared/content-video-panel/content-video-panel.component.ts
+++ src/app/shared/content-video-panel/content-video-panel.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Content } from 'src/app/app.models';
+import { Content, Exam, Collections } from 'src/app/app.models';
 import { VgAPI } from 'videogular2/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'epsi-content-video-panel',
@@ -17,12 +20,15 @@ export class ContentVideoPanelComponent implements OnInit {
   public get seekTime() { return this._seek }
 
   private videoApi: VgAPI
+  public preclase$: Observable<Exam>
 
   constructor(
-    public router: Router
+    public router: Router,
+    private afs: AngularFirestore
   ) { }
 
   ngOnInit() {
+    this.getExamByContentName(this.content.name).subscribe(exam => exam ? this.router.navigate(['/exam/preclase', exam.id]) : null)
   }
 
   onPlayerReady(api: VgAPI) {
@@ -35,6 +41,13 @@ export class ContentVideoPanelComponent implements OnInit {
     this.videoApi.play()
     this.videoApi.seekTime(time)
     this._seek = null
+  }
+
+  getExamByContentName(name: string): Observable<Exam> {
+    return this.afs.collection<Exam>(Collections.EXAM, ref => ref
+      .where('name', '==', name))
+      .valueChanges()
+      .pipe(map(exams => exams ? exams[0] : null))
   }
 
 }
