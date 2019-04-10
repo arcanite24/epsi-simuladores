436c9029f47c5ef41f63e4acbcd3e127b4c035e4
Brandon G. Neri <arcaniteamp@gmail.com>
2019-04-10 12:20:46 -0500

    EpsiDev Galaxy Automatic Deploy

--- src/app/shared/user-roles/user-roles.component.ts
+++ src/app/shared/user-roles/user-roles.component.ts
@@ -17,7 +17,7 @@ export class UserRolesComponent implements OnInit {
   public esencialModel: string[] = EsencialModel
   public adminModel: string[] = [ Roles.Admin ]
   public tempranoModel: string[] = TempranoModel
-  public presencialModel: string[] = [ Roles.Presencial ]
+  public presencialModel: string[] = [ ...EsencialModel, Roles.Presencial ]
 
   public l: boolean = false
 
