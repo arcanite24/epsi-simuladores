befca791ef71aa2e228b9b3d50dd7cb645d3a212
Brandon G. Neri <arcaniteamp@gmail.com>
2019-04-24 16:22:44 -0500

    EpsiDev Galaxy Automatic Deploy

--- src/app/shared/content-video-panel/content-video-panel.component.ts
+++ src/app/shared/content-video-panel/content-video-panel.component.ts
@@ -40,11 +40,21 @@ export class ContentVideoPanelComponent implements OnInit {
 
   async loadPreclase() {
 
-    const exam = await this.getExamByContentName(this.content.name)
+    /*const exam = await this.getExamByContentName(this.content.name)*/
+    const exam = await this.afs.collection<Exam>(Collections.EXAM, ref => ref
+      .where('content.id', '==', this.content.id)
+      .where('type', '==', ExamTypes.PRECLASE))
+      .valueChanges()
+      .pipe(
+        take(1),
+        map(exams => exams ? exams[0] : null),
+      )
+      .toPromise();
 
     if (exam) {
       const resuelto = await this.preclaseResuelto(this.auth.user.uid, exam.id)
       this.examId = exam.id
+      console.log('preclase', exam, 'resuelto:', resuelto);
       if (!resuelto) this.modal.getModal('preclaseExamModal').open()
     }
 
