6ad70c589ab819e94d14cb440576b94ef191317d
Brandon G. Neri <arcaniteamp@gmail.com>
2019-06-10 15:33:02 -0500

    EpsiDev Galaxy Automatic Deploy

--- src/app/shared/plan-selector-panel/plan-selector-panel.component.ts
+++ src/app/shared/plan-selector-panel/plan-selector-panel.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { NgbDate, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import moment from 'moment'
+import moment from 'moment';
 import { ToastrService } from 'ngx-toastr';
 import { Content, Collections } from 'src/app/app.models';
-import { sortBy, findIndex, flattenDeep } from 'lodash'
+import { sortBy, findIndex, flattenDeep } from 'lodash';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from 'src/app/services/auth.service';
 import { NgxSmartModalService } from 'ngx-smart-modal';
-let twix = require('twix');
+const twix = require('twix');
 
 @Component({
   selector: 'epsi-plan-selector-panel',
@@ -18,19 +18,19 @@ let twix = require('twix');
 })
 export class PlanSelectorPanelComponent implements OnInit {
 
-  public step: string = 'mode_selector'
-  public mode: string
-  public finishDate: NgbDate
+  public step = 'mode_selector';
+  public mode: string;
+  public finishDate: NgbDate;
 
-  public loading: boolean = false
-  public loadingText: string = 'Iniciando Calendario Inteligente Zamná...'
+  public loading = false;
+  public loadingText = 'Iniciando Calendario Inteligente Zamná...';
 
-  public maxDate: NgbDate = new NgbDate(2018, 8, 30)
-  public minDate: NgbDate = new NgbDate(new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDay())
+  public maxDate: NgbDate = new NgbDate(2018, 8, 30);
+  public minDate: NgbDate = new NgbDate(new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDay());
 
-  public content: Content[]
-  public dias: number
-  public range: any
+  public content: Content[];
+  public dias: number;
+  public range: any;
 
   constructor(
     private data: DataService,
@@ -45,103 +45,104 @@ export class PlanSelectorPanelComponent implements OnInit {
   }
 
   setMode(mode: string) {
-    this.mode = mode
-    if (mode == 'ignore') return this.setIgnoreMode();
-    if (mode == 'date_order') this.modal.getModal('dateSelector').open()
-    if (mode == 'only_date') this.modal.getModal('dateSelector').open()
+    this.mode = mode;
+    if (mode == 'ignore') { return this.setIgnoreMode(); }
+    if (mode == 'date_order') { this.modal.getModal('dateSelector').open() }
+    if (mode == 'only_date') { this.modal.getModal('dateSelector').open() }
   }
 
   async setIgnoreMode() {
-    await this.afs.collection(Collections.USER).doc(this.auth.user.uid).update({noCalendar: true})
+    await this.afs.collection(Collections.USER).doc(this.auth.user.uid).update({noCalendar: true});
   }
 
   async generateCalendar(date: NgbDate) {
-  
-    this.modal.getModal('dateSelector').close()
-    this.loading = true
-    
-    const finish = new Date(date.year, date.month - 1, date.day).toISOString()
-    const range = (moment() as any).twix(finish)
-    console.log(finish)
 
-    if (!range.isValid()) return this.toastr.error('Selecciona una fecha válida')
-    if (range.isPast()) return this.toastr.error('Selecciona una fecha válida')
-    const dias = range.count('days')
-    console.log('repartiendo contenido en', dias, 'dias')
+    this.modal.getModal('dateSelector').close();
+    this.loading = true;
 
-    this.loadingBar.start()
-    this.loadingText = 'Cargando contenido...'
-    this.modal.getModal('contentFilterModal').open()
+    const finish = new Date(date.year, date.month - 1, date.day).toISOString();
+    const range = (moment() as any).twix(finish);
+    console.log(finish);
 
-    this.content = await this.getFormattedContent()
-    
-    if (this.mode == 'only_date') {
-      this.setCalendarToUser(this.content, dias, range)
-    } else if (this.mode == 'date_order') {
-      this.loadingText = 'Seleccionando contenido'
-      this.loadingBar.complete()
-      this.range = range
-      this.dias = dias
+    if (!range.isValid()) { return this.toastr.error('Selecciona una fecha válida') }
+    if (range.isPast()) { return this.toastr.error('Selecciona una fecha válida') }
+    const dias = range.count('days');
+    console.log('repartiendo contenido en', dias, 'dias');
+
+    this.loadingBar.start();
+    this.loadingText = 'Cargando contenido...';
+    this.modal.getModal('contentFilterModal').open();
+
+    this.content = await this.getFormattedContent();
+
+    if (this.mode === 'only_date') {
+      this.setCalendarToUser(this.content, dias, range);
+    } else if (this.mode === 'date_order') {
+      this.loadingText = 'Seleccionando contenido';
+      this.loadingBar.complete();
+      this.range = range;
+      this.dias = dias;
     }
 
   }
 
   async contentFilterCallback(content: Content[], dias: number, range: any) {
 
-    const _content = content.filter(c => c.selected)
-    console.log(dias, _content)
+    const _content = content.filter(c => c.selected);
+    console.log(dias, _content);
 
-    await this.setCalendarToUser(_content, dias, range)
+    await this.setCalendarToUser(_content, dias, range);
 
   }
 
   async setCalendarToUser(content: Content[], dias: number, range: any) {
 
-    content = flattenDeep(content.map(c => c.temas)) as Content[]
+    content = flattenDeep(content.map(c => c.temas)) as Content[];
 
     content = content
-      .filter(c => c.type == 'tema')
+      .filter(c => c.type === 'tema')
       .filter(c => !c.ignoreOnSmartCalendar)
       .filter(c => c.video);
     let calendar = [];
 
-    const cociente = content.length / dias
+    const cociente = content.length / dias;
     let perDay = cociente - Math.floor(cociente) > 0.5 ? Math.ceil(cociente) : Math.floor(cociente);
-    perDay = perDay < 1 ? 1 : perDay
-    console.log(`repartiendo ${content.length} temas en ${dias} dias, ${perDay} temas por día`)
-    this.loadingText = `Repartiendo ${content.length} temas en ${dias} dias, ${perDay} temas por día.`
+    perDay = perDay < 1 ? 1 : perDay;
+    console.log(`repartiendo ${content.length} temas en ${dias} dias, ${perDay} temas por día`);
+    this.loadingText = `Repartiendo ${content.length} temas en ${dias} dias, ${perDay} temas por día.`;
 
-    const iter = range.iterate('days')
+    const iter = range.iterate('days');
     while (iter.hasNext()) {
-      
-      const dia = iter.next()
-      const newTemas = content.splice(0, perDay)
-      this.loadingText = `Temas para el ${dia.format('YYYY/MM/DD')}`
+
+      const dia = iter.next();
+      const newTemas = content.splice(0, perDay);
+      this.loadingText = `Temas para el ${dia.format('YYYY/MM/DD')}`;
 
       calendar = [...calendar, newTemas.map(t => ({
         start: dia.toISOString(),
         fullDay: true,
-        event: t.event,
-        content: t.id,
-        content_name: t.name,
-      }))]
+        event: t.event || null,
+        content: t.id || null,
+        content_name: t.name || null,
+      }))];
 
     }
 
-    let customCalendar = {}
-    this.loadingText = 'Guardando calendario personalizado'
+    const customCalendar = {};
+    this.loadingText = 'Guardando calendario personalizado';
 
     for (const [i, calendarDay] of Object.entries(calendar)) {
-      customCalendar[i] = calendarDay
+      customCalendar[i] = calendarDay;
     }
 
-    console.log(customCalendar)
+    console.log(customCalendar, this.auth.user.uid);
+    const uid = this.auth.user.uid;
 
-    await this.afs.collection(Collections.USER).doc(this.auth.user.uid).update({customCalendar: null})
-    await this.afs.collection(Collections.USER).doc(this.auth.user.uid).update({customCalendar})
+    await this.afs.collection(Collections.USER).doc(uid).update({customCalendar: null});
+    await this.afs.collection(Collections.USER).doc(uid).update({customCalendar});
 
-    this.loadingText = 'Calendario completado'
-    this.loadingBar.complete()
+    this.loadingText = 'Calendario completado';
+    this.loadingBar.complete();
 
     // TODO: Temp fix for the calendar event loading bug
     location.reload();
@@ -150,52 +151,52 @@ export class PlanSelectorPanelComponent implements OnInit {
 
   async getFormattedContent(): Promise<Content[]> {
 
-    const cache = localStorage.getItem('zamna-content-cache')
-    if (cache && this.auth.isAdmin) return JSON.parse(cache);
+    const cache = localStorage.getItem('zamna-content-cache');
+    if (cache && this.auth.isAdmin) { return JSON.parse(cache); }
 
-    let content = []
+    let content = [];
 
-    const _materias = await this.data.getCollectionQuery<Content>(Collections.CONTENT, ref => ref.where('type', '==', 'materia'))
-    const materias = _materias.filter(m => !m.name.includes('Temprano'))
+    const _materias = await this.data.getCollectionQuery<Content>(Collections.CONTENT, ref => ref.where('type', '==', 'materia'));
+    const materias = _materias.filter(m => !m.name.includes('Temprano'));
 
     for (const materia of materias) {
 
-      console.log('materia:', materia.name)
-      this.loadingText = materia.name
+      console.log('materia:', materia.name);
+      this.loadingText = materia.name;
 
       const bloques = await this.data.getCollectionQuery<Content>(Collections.CONTENT, ref => ref
         .where('type', '==', 'bloque')
-        .where('parent_id', '==', materia.id))
+        .where('parent_id', '==', materia.id));
 
-      const _bloques = sortBy(bloques, 'sortIndex')
+      const _bloques = sortBy(bloques, 'sortIndex');
 
       for (const bloque of _bloques) {
 
-        this.loadingText = bloque.name
+        this.loadingText = bloque.name;
 
         /*content = [...content, bloque]*/
-        
+
         const temas = await this.data.getCollectionQuery<Content>(Collections.CONTENT, ref => ref
           .where('type', '==', 'tema')
-          .where('parent_id', '==', bloque.id))
+          .where('parent_id', '==', bloque.id));
 
-        bloque.temas = sortBy(temas, 'sortIndex').map(t => ({...t, parent_name: bloque.name, selected: true}))
+        bloque.temas = sortBy(temas, 'sortIndex').map(t => ({...t, parent_name: bloque.name, selected: true}));
 
-        content = [...content, bloque]
+        content = [...content, bloque];
 
       }
 
     }
 
-    const final = content.map(c => ({...c, selected: true}))
+    const final = content.map(c => ({...c, selected: true}));
     localStorage.setItem('zamna-content-cache', JSON.stringify(final));
-    return final
+    return final;
 
   }
 
   radioChange(c: Content) {
     if (c.type == 'bloque') {
-      c.temas = c.temas.map(t => ({...t, selected: c.selected}))
+      c.temas = c.temas.map(t => ({...t, selected: c.selected}));
       /*c.selected ? this.content = this.content.map(cc => {
         return cc.parent_id == c.id ? ({...cc, selected: true}) : cc
       }) : this.content = this.content.map(cc => {
@@ -206,7 +207,7 @@ export class PlanSelectorPanelComponent implements OnInit {
 
   orderBloque(content: Content[] = [], current: Content, i: number, delta: number) {
 
-    this.content = this.move(content, i, i + delta)
+    this.content = this.move(content, i, i + delta);
 
     /*const trimmed = content.slice(i + 1)
     const lastChildRelative = findIndex(trimmed, c => c.type == 'bloque') - 1
