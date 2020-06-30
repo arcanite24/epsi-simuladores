1cb65e4668246955ed4b3394998c089dcef406e6
Brandon G. Neri <arcaniteamp@gmail.com>
2020-06-29 20:46:38 -0500

    .

--- src/app/shared/payment-model-panel/payment-model-panel.component.ts
+++ src/app/shared/payment-model-panel/payment-model-panel.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { PaymentModel, Collections, PaymentStatus, Coupon, PaymentPack, TuGuiaStats, TuGuiaStatsText, PaymentModelType, ExtraUnlock } from 'src/app/app.models';
-import { DomSanitizer } from '@angular/platform-browser';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { ToastrService } from 'ngx-toastr';
-import { PaymentService } from 'src/app/services/payment.service';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { ActivatedRoute, Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
-import { take, map, tap } from 'rxjs/operators';
-import { AuthService } from 'src/app/services/auth.service';
-import * as firebase from 'firebase/app';
-import { Observable } from 'rxjs';
-import { NgxSmartModalService } from 'ngx-smart-modal';
-import { findIndex, flattenDeep } from 'lodash';
+import { Component, OnInit, Input } from "@angular/core";
+import {
+  PaymentModel,
+  Collections,
+  PaymentStatus,
+  Coupon,
+  PaymentPack,
+  TuGuiaStats,
+  TuGuiaStatsText,
+  PaymentModelType,
+  ExtraUnlock,
+} from "src/app/app.models";
+import { DomSanitizer } from "@angular/platform-browser";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { ToastrService } from "ngx-toastr";
+import { PaymentService } from "src/app/services/payment.service";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { ActivatedRoute, Router } from "@angular/router";
+import { environment } from "src/environments/environment";
+import { take, map, tap } from "rxjs/operators";
+import { AuthService } from "src/app/services/auth.service";
+import * as firebase from "firebase/app";
+import { Observable } from "rxjs";
+import { NgxSmartModalService } from "ngx-smart-modal";
+import { findIndex, flattenDeep } from "lodash";
 
 @Component({
-  selector: 'epsi-payment-model-panel',
-  templateUrl: './payment-model-panel.component.html',
-  styleUrls: ['./payment-model-panel.component.scss']
+  selector: "epsi-payment-model-panel",
+  templateUrl: "./payment-model-panel.component.html",
+  styleUrls: ["./payment-model-panel.component.scss"],
 })
 export class PaymentModelPanelComponent implements OnInit {
-
   @Input() model: PaymentModel;
   @Input() disabled = false;
 
@@ -60,12 +69,11 @@ export class PaymentModelPanelComponent implements OnInit {
     private afAuth: AngularFireAuth,
     private router: Router,
     private auth: AuthService,
-    private modal: NgxSmartModalService,
-  ) { }
+    private modal: NgxSmartModalService
+  ) {}
 
   ngOnInit() {
-
-    console.log(this.model)
+    console.log(this.model);
 
     if (this.model.type === PaymentModelType.Apunte) {
       this.apunteSelection[this.model.id] = true;
@@ -79,53 +87,68 @@ export class PaymentModelPanelComponent implements OnInit {
       this.simuladorSelection = this.model.id;
     }
 
-    this.guias$ = this.afs.collection<PaymentModel>(Collections.PAYMENT_MODEL, ref => ref
-      .where('type', '==', PaymentModelType.Guia))
+    this.guias$ = this.afs
+      .collection<PaymentModel>(Collections.PAYMENT_MODEL, (ref) =>
+        ref.where("type", "==", PaymentModelType.Guia)
+      )
       .valueChanges()
-      .pipe(tap(models => {
-        this.guias = models;
-        if (!this.model.canBuyIndividual) {
-          console.log(models)
-          models.forEach(m => this.guiaSelection[m.id] = true);
-        }
-      }));
+      .pipe(
+        tap((models) => {
+          this.guias = models;
+          if (!this.model.canBuyIndividual) {
+            console.log(models);
+            models.forEach((m) => (this.guiaSelection[m.id] = true));
+          }
+        })
+      );
 
-    this.apuntes$ = this.afs.collection<PaymentModel>(Collections.PAYMENT_MODEL, ref => ref
-      .where('type', '==', PaymentModelType.Apunte))
+    this.apuntes$ = this.afs
+      .collection<PaymentModel>(Collections.PAYMENT_MODEL, (ref) =>
+        ref.where("type", "==", PaymentModelType.Apunte)
+      )
       .valueChanges()
-      .pipe(tap(models => {
-        this.apuntes = models;
-        if (!this.model.canBuyIndividual) {
-          models.forEach(m => this.apunteSelection[m.id] = true);
-        }
-      }));
+      .pipe(
+        tap((models) => {
+          this.apuntes = models;
+          if (!this.model.canBuyIndividual) {
+            models.forEach((m) => (this.apunteSelection[m.id] = true));
+          }
+        })
+      );
 
-    this.simuladores$ = this.afs.collection<PaymentModel>(Collections.PAYMENT_MODEL, ref => ref
-      .where('type', '==', PaymentModelType.Simulador))
+    this.simuladores$ = this.afs
+      .collection<PaymentModel>(Collections.PAYMENT_MODEL, (ref) =>
+        ref.where("type", "==", PaymentModelType.Simulador)
+      )
       .valueChanges()
-      .pipe(tap(models => {
-        this.simuladores = models;
-        if (!this.model.canBuyIndividual) {
-          models.forEach(m => this.simuladorSelection[m.id] = true);
-        }
-      }));
-
+      .pipe(
+        tap((models) => {
+          this.simuladores = models;
+          if (!this.model.canBuyIndividual) {
+            models.forEach((m) => (this.simuladorSelection[m.id] = true));
+          }
+        })
+      );
   }
 
   get modelBody() {
-    if (!this.model) { return '...'; }
+    if (!this.model) {
+      return "...";
+    }
     return this.sanitizer.bypassSecurityTrustHtml(this.model.desc);
   }
 
   get subtotal(): number {
-    return +this.getGuiasTotal(this.guias, this.guiaSelection) +
+    return (
+      +this.getGuiasTotal(this.guias, this.guiaSelection) +
       +this.getApuntesTotal(this.apuntes, this.apunteSelection) +
-      +this.getSimuladorTotal(this.simuladores, this.simuladorSelection);
+      +this.getSimuladorTotal(this.simuladores, this.simuladorSelection)
+    );
   }
 
   get total(): number {
-
-    let total = +this.getGuiasTotal(this.guias, this.guiaSelection) +
+    let total =
+      +this.getGuiasTotal(this.guias, this.guiaSelection) +
       +this.getApuntesTotal(this.apuntes, this.apunteSelection) +
       +this.getSimuladorTotal(this.simuladores, this.simuladorSelection);
 
@@ -133,34 +156,49 @@ export class PaymentModelPanelComponent implements OnInit {
       total *= 0.9;
     }
 
-    if (this.apuntes && this.totalSelectedApuntes === this.apuntes.length) {
+    if (this.apuntes && this.totalSelectedApuntes >= 7) {
       total *= 0.9;
     }
 
     return total;
-
   }
 
   get totalSelectedGuias(): number {
-    return Object.values(this.guiaSelection).filter(guia => guia).length;
+    return Object.values(this.guiaSelection).filter((guia) => guia).length;
   }
 
   get totalSelectedApuntes(): number {
-    return Object.values(this.apunteSelection).filter(guia => guia).length;
+    return Object.values(this.apunteSelection).filter((guia) => guia).length;
   }
 
   get haveDiscount(): boolean {
-    if (this.totalSelectedGuias >= 7) { return true; }
-    if (this.apuntes && this.totalSelectedApuntes === this.apuntes.length) { return true; }
+    if (this.totalSelectedGuias >= 7) {
+      return true;
+    }
+    if (this.apuntes && this.totalSelectedApuntes >= 7) {
+      return true;
+    }
     return false;
   }
 
   getGuiasTotal(guias: PaymentModel[] = [], selected: any = {}) {
-    return +parseFloat(guias.filter(guia => selected[guia.id]).map(guia => +guia.amount).reduce((a, b) => a + b, 0).toString());
+    return +parseFloat(
+      guias
+        .filter((guia) => selected[guia.id])
+        .map((guia) => +guia.amount)
+        .reduce((a, b) => a + b, 0)
+        .toString()
+    );
   }
 
   getApuntesTotal(apuntes: PaymentModel[] = [], selected: any = {}) {
-    return +parseFloat(apuntes.filter(guia => selected[guia.id]).map(guia => +guia.amount).reduce((a, b) => a + b, 0).toString());
+    return +parseFloat(
+      apuntes
+        .filter((guia) => selected[guia.id])
+        .map((guia) => +guia.amount)
+        .reduce((a, b) => a + b, 0)
+        .toString()
+    );
   }
 
   getSimuladorTotal(simuladores: PaymentModel[] = [], selected: string) {
@@ -168,7 +206,7 @@ export class PaymentModelPanelComponent implements OnInit {
       return 0;
     }
 
-    const index = findIndex(simuladores, sim => sim.id === selected);
+    const index = findIndex(simuladores, (sim) => sim.id === selected);
 
     if (index > -1) {
       return +simuladores[index].amount;
@@ -180,40 +218,46 @@ export class PaymentModelPanelComponent implements OnInit {
   loadGuiaAd(modal: string, guia: string) {
     this.extraSelector = false;
     this.guiaSelection[guia] = true;
-    this.guiaSelection['27B61yUUWl1LclcAOX6s'] = true;
-    this.guiaSelection['2CzOTpyqGdmloV5ErsVw'] = true;
-    this.guiaSelection['JtNUDnZC4ZnH0vMkPjgv'] = true;
-    this.guiaSelection['RllNakz2MyDYsFLCnCpJ'] = true;
-    this.guiaSelection['cb2h4zllQozflSFvrZrU'] = true;
-    this.guiaSelection['gYgbU8ga13W4bAqzUP5K'] = true;
+    this.guiaSelection["27B61yUUWl1LclcAOX6s"] = true;
+    this.guiaSelection["2CzOTpyqGdmloV5ErsVw"] = true;
+    this.guiaSelection["JtNUDnZC4ZnH0vMkPjgv"] = true;
+    this.guiaSelection["RllNakz2MyDYsFLCnCpJ"] = true;
+    this.guiaSelection["cb2h4zllQozflSFvrZrU"] = true;
+    this.guiaSelection["gYgbU8ga13W4bAqzUP5K"] = true;
     this.modal.getModal(modal).close();
   }
 
   loadApunteAd() {
-    this.modal.getModal('apunteModal').close();
-    const apuntes = this.apuntes.map(apunte => apunte.id);
+    this.modal.getModal("apunteModal").close();
+    const apuntes = this.apuntes.map((apunte) => apunte.id);
     for (const apunte of apuntes) {
       this.apunteSelection[apunte] = true;
     }
   }
 
   async generatePayment(model: PaymentModel) {
-
-    if (this.loading) { return this.toastr.error('No puedes generar otra solicitud de pago...'); }
+    if (this.loading) {
+      return this.toastr.error("No puedes generar otra solicitud de pago...");
+    }
     this.loading = true;
 
     const user = this.afAuth.auth.currentUser;
-    const email = environment.production === true ? user.email : 'test_user_41665327@testuser.com';
+    const email =
+      environment.production === true
+        ? user.email
+        : "test_user_41665327@testuser.com";
 
     try {
-
       if (this.selectedStat) {
-        await this.afs.collection(Collections.Stat).doc(this.selectedStat).update({ counter: firebase.firestore.FieldValue.increment(1) });
+        await this.afs
+          .collection(Collections.Stat)
+          .doc(this.selectedStat)
+          .update({ counter: firebase.firestore.FieldValue.increment(1) });
       }
 
       const request_payload = {
         coupon: null,
-        coupon_value: null
+        coupon_value: null,
       };
 
       let isFullCoupon = false;
@@ -221,28 +265,30 @@ export class PaymentModelPanelComponent implements OnInit {
 
       // Coupon validation
       if (this.coupon && this.coupon.length > 5) {
-
-        const discount = await this.afs.collection<Coupon>(Collections.COUPON, ref => ref.where('code', '==', this.coupon))
+        const discount = await this.afs
+          .collection<Coupon>(Collections.COUPON, (ref) =>
+            ref.where("code", "==", this.coupon)
+          )
           .valueChanges()
           .pipe(
             take(1),
-            map(coupons => coupons[0])
-          ).toPromise();
+            map((coupons) => coupons[0])
+          )
+          .toPromise();
 
         if (!discount) {
           this.loading = false;
-          return this.toastr.error('El cupón no es válido...');
+          return this.toastr.error("El cupón no es válido...");
         }
 
         if (discount.used) {
           this.loading = false;
-          return this.toastr.error('El cupón ya ha sido usado...');
+          return this.toastr.error("El cupón ya ha sido usado...");
         }
 
         request_payload.coupon = discount.id;
         request_payload.coupon_value = discount.value / 100;
         isFullCoupon = discount.value >= 100;
-
       }
 
       // Create payment request
@@ -257,15 +303,21 @@ export class PaymentModelPanelComponent implements OnInit {
         status: isFullCoupon ? PaymentStatus.Approved : PaymentStatus.Pending,
         delivered: false,
         wasFullCoupon: isFullCoupon,
-        pack: this.pack ? this.pack : null
+        pack: this.pack ? this.pack : null,
       });
 
-      const selectedSim = this.simuladores.filter(s => s.id === this.simuladorSelection)[0];
+      const selectedSim = this.simuladores.filter(
+        (s) => s.id === this.simuladorSelection
+      )[0];
 
       const extraUnlock = flattenDeep([
-        ...this.guias.filter(guia => this.guiaSelection[guia.id] === true).map(g => g.unlocks),
-        ...this.apuntes.filter(apunte => this.apunteSelection[apunte.id] === true).map(g => g.unlocks),
-        ...selectedSim ? selectedSim.unlocks : [],
+        ...this.guias
+          .filter((guia) => this.guiaSelection[guia.id] === true)
+          .map((g) => g.unlocks),
+        ...this.apuntes
+          .filter((apunte) => this.apunteSelection[apunte.id] === true)
+          .map((g) => g.unlocks),
+        ...(selectedSim ? selectedSim.unlocks : []),
       ]);
 
       const extraUnlockPayload: ExtraUnlock = {
@@ -275,7 +327,10 @@ export class PaymentModelPanelComponent implements OnInit {
         unlocks: extraUnlock as string[],
       };
 
-      await this.afs.collection(Collections.ExtraUnlock).doc(extraUnlockPayload.id).set({ ...extraUnlockPayload });
+      await this.afs
+        .collection(Collections.ExtraUnlock)
+        .doc(extraUnlockPayload.id)
+        .set({ ...extraUnlockPayload });
 
       console.log(extraUnlock, extraUnlockPayload);
 
@@ -284,7 +339,6 @@ export class PaymentModelPanelComponent implements OnInit {
       }
 
       if (isFullCoupon) {
-
         // give extra unlock
         const extraPayload = {};
 
@@ -292,50 +346,61 @@ export class PaymentModelPanelComponent implements OnInit {
           extraPayload[role] = true;
         }
 
-        await this.afs.collection(Collections.USER).doc(user.uid).update({ ...extraPayload });
+        await this.afs
+          .collection(Collections.USER)
+          .doc(user.uid)
+          .update({ ...extraPayload });
 
         console.log({ ...user });
-        const _user = await this.afs.doc(`${Collections.USER}/${user.uid}`)
+        const _user = await this.afs
+          .doc(`${Collections.USER}/${user.uid}`)
           .valueChanges()
-          .pipe(
-            take(1)
-          ).toPromise();
+          .pipe(take(1))
+          .toPromise();
 
         // Update coupon registry
-        await this.afs.doc(`${Collections.COUPON}/${request_payload.coupon}`).update({
-          date: new Date().toISOString(),
-          user: { ..._user },
-          used: true
-        });
+        await this.afs
+          .doc(`${Collections.COUPON}/${request_payload.coupon}`)
+          .update({
+            date: new Date().toISOString(),
+            user: { ..._user },
+            used: true,
+          });
 
         // If pack is selected generate coupons
         // TODO: Actually implement this, soooo boring and probably none will gonna use EVER
 
         // Ignore Payment Gateway and redirect to Payment Request Detail
-        return this.router.navigate(['/pago/status', request_id]);
-
+        return this.router.navigate(["/pago/status", request_id]);
       }
 
       amount = this.pack ? this.pack.price : this.total;
-      if (request_payload.coupon) { amount -= amount * request_payload.coupon_value; }
+      if (request_payload.coupon) {
+        amount -= amount * request_payload.coupon_value;
+      }
 
-      const paymentInfo = await this.payment
-        .generatePaymentUrl(model.id, request_id, model.name, amount, email, environment.production === true);
+      const paymentInfo = await this.payment.generatePaymentUrl(
+        model.id,
+        request_id,
+        model.name,
+        amount,
+        email,
+        environment.production === true
+      );
       this.payment_url = paymentInfo.init_point;
       console.log(paymentInfo);
       window.location.href = this.payment_url;
-      setTimeout(() => this.loading = false, 3000);
-
+      setTimeout(() => (this.loading = false), 3000);
     } catch (error) {
       console.log(error);
-      this.toastr.error('Ocurrió un error al generar tu pago, por favor intenta más tarde o contacta con un administrador.');
+      this.toastr.error(
+        "Ocurrió un error al generar tu pago, por favor intenta más tarde o contacta con un administrador."
+      );
       this.loading = false;
     }
-
   }
 
   async loadPaypalButton(model: PaymentModel) {
-
     const request_payload = {
       coupon: null,
       coupon_value: null,
@@ -345,59 +410,65 @@ export class PaymentModelPanelComponent implements OnInit {
 
     // Coupon validation
     if (this.coupon && this.coupon.length > 5) {
-
-      const discount = await this.afs.collection<Coupon>(Collections.COUPON, ref => ref.where('code', '==', this.coupon))
+      const discount = await this.afs
+        .collection<Coupon>(Collections.COUPON, (ref) =>
+          ref.where("code", "==", this.coupon)
+        )
         .valueChanges()
         .pipe(
           take(1),
-          map(coupons => coupons[0])
-        ).toPromise();
+          map((coupons) => coupons[0])
+        )
+        .toPromise();
 
       if (!discount) {
         this.loading = false;
-        return this.toastr.error('El cupón no es válido...');
+        return this.toastr.error("El cupón no es válido...");
       }
 
       if (discount.used) {
         this.loading = false;
-        return this.toastr.error('El cupón ya ha sido usado...');
+        return this.toastr.error("El cupón ya ha sido usado...");
       }
 
       request_payload.coupon = discount.id;
       request_payload.coupon_value = discount.value / 100;
       isFullCoupon = discount.value >= 100;
-
     }
 
     if (isFullCoupon) {
       this.loading = false;
-      return this.toastr.error('El cupón es del 100%, no uses PayPal para redimir el cupón.');
+      return this.toastr.error(
+        "El cupón es del 100%, no uses PayPal para redimir el cupón."
+      );
     }
 
-    const amount = request_payload.coupon_value ? model.amount - model.amount * request_payload.coupon_value : model.amount;
+    const amount = request_payload.coupon_value
+      ? model.amount - model.amount * request_payload.coupon_value
+      : model.amount;
 
-    this.payment.generatePaypalButton(`paypal-container-${model.id}`, amount, async payment => {
-      console.log(model);
-      if (payment.state == 'approved') {
+    this.payment.generatePaypalButton(
+      `paypal-container-${model.id}`,
+      amount,
+      async (payment) => {
+        console.log(model);
+        if (payment.state == "approved") {
+          console.log("pago de paypal aprobado");
+          const uid = this.auth.user.uid;
+          const payload = {};
 
-        console.log('pago de paypal aprobado');
-        const uid = this.auth.user.uid;
-        const payload = {};
+          for (const role of model.unlocks) {
+            payload[role] = true;
+          }
 
-        for (const role of model.unlocks) {
-          payload[role] = true;
+          await this.afs.collection(Collections.USER).doc(uid).update(payload);
+          this.router.navigate(["/home"]);
+        } else {
+          this.toastr.error("Algo salio mal con tu pago de PayPal");
         }
-
-        await this.afs.collection(Collections.USER).doc(uid).update(payload);
-        this.router.navigate(['/home']);
-
-      } else {
-        this.toastr.error('Algo salio mal con tu pago de PayPal');
       }
-    });
+    );
 
     this.showPaypal = true;
-
   }
-
 }
